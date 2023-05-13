cd4ede926562acbbd213d7c0834b452074476edb
An <anaislcno@gmail.com>
2023-05-13 16:09:28 +0200

    add media queries on housing sheet 2

--- src/components/HostCard/index.jsx
+++ src/components/HostCard/index.jsx
@@ -3,12 +3,10 @@ import "../HostCard/HostCard.scss"
 
 function HostCard ({ host }) {
     return (
-      <section>
         <div className="host-container">
           <h2 className="host-card__name">{host.name}</h2>
           <img className="host-card__img" src={host.picture} alt={host.name} />
         </div>
-      </section>
     );
   }
 
